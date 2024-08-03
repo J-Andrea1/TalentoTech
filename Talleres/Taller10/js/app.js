d8e360c6f1836f3d6a7ef8fd3c9a7ce9bdd2d71f
Andrea Villegas <jeimy.villegas@correounivalle.edu.co>
2024-08-02 19:22:31 -0500

    taller 10 completo

--- Talleres/Taller10/js/app.js
+++ Talleres/Taller10/js/app.js
@@ -1,8 +1,8 @@
-//Punto 1
+/*//Punto 1
 let nombre = "Jeimy";
-let edad = 33;
+let edad = 34;
 document.write("Tu nombre es " + nombre + " y tu edad es: " + edad + "<br>" + "<br>");
-
+*/
 // Punto 2
 let esEstudiante = true;
 let altura = 165;
@@ -27,7 +27,7 @@ modulo = a % b;
 alert("El resultado de a + b es: " + suma + "\n" + "El resultado de a - b es: " + resta
     + "\n" + "El resultado de a * b es: " + multiplicacion + "\n" + "El resultado de a / b es: " + division
     + "\n" + "El resultado de a % b es: " + modulo);
-
+/*
 //Punto 5
 let x = 10;
 x += 10;
@@ -36,7 +36,7 @@ x *= 10;
 x /= 10;
 x %= 10;
 document.write("El valor de x es: " + x + "<br>" + "<br>");
-
+*/
 // Punto 6
 let contador1 = 0;
 let contador2 = 1;
@@ -53,3 +53,64 @@ alert("p es mayor que q: " + (p > q) + "\n" + "p es menor que q: " + (p < q) + "
 "\n" + "p es menor igual que q: " + (p <= q) + "\n" + "p es == que q: " + (p == q) + "\n" + "p es === que q: " + (p === q));
 
 // Punto 8
+let x = true;
+let y = false;
+let z = false;
+
+alert("El resultado de x && y es: " + (x && y) + "\n" + "El resultado de x || y es: " + (x || y) + "\n" + "El resultado de !x es: " +
+ (!x) + "\n" + "El resultado de x && y && z es: " + (x && y && z));
+
+ // Punto 9
+ let nombre = "Jeimy";
+ let apellido = "Villegas";
+ alert("Tu nombre completo es: " + nombre + " " + apellido);
+
+ // Punto 10
+ let numero = 34;
+ let cadena = numero.toString();
+
+ alert("El número en formato de cadena es: " + cadena + " " + (numero + cadena));
+
+ // Punto 11
+ let distanciaKm = 70;
+ let distanciaM = distanciaKm*1000;
+ let distanciaCm = distanciaKm*100000;
+ let distanciaMilla = distanciaKm/1.609344;
+ 
+ alert(distanciaKm + " km en metros es: " + distanciaM + "\n" + distanciaKm + " km en centimetros es: " + distanciaCm + "\n" + 
+    distanciaKm + " km en millas es: " + distanciaMilla);
+
+// Punto 12
+let base = 10;
+let altura1 = 15;
+let altura2 = 15;
+
+let perimetro = (base+altura1+altura2);
+let area = (base * altura1) / 2;
+
+alert("El área del triángulo es: " + area + "\n" + "El perimetro es: " + perimetro);
+
+//punto 13
+let calificacion1 = 4;
+let calificacion2 = 5;
+let calificacion3 = 5;
+
+let promedio = (calificacion1 + calificacion2 + calificacion3) / 3;
+alert("El promedio de las calificaciones es: " + promedio); 
+
+//punto 14
+let precioOriginal = 8500;
+let descuento = (15/100);
+let precioFinal =(descuento * precioOriginal);
+
+alert("El precio final con descuento es: " + precioFinal);
+
+//punto 15
+let dolares = 4;
+let euros = 92;
+let librasEsterlinas = 78;
+let yenes = 146;
+
+alert(dolares + " " + "dolares a euros son: " + (dolares*euros)+"\n"+
+    dolares + " "  + "dolares a libras esterlinas son: " + (dolares*librasEsterlinas)+"\n"+
+    dolares + " " + "dolares a yenes son: " + (dolares*yenes));
